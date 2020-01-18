def1e36193094eb328117fa91ee12eb3ae6ae321
Mario Adamo Jr <marioadamojr@gmail.com>
2020-01-18 21:02:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function fetchBooks() {
+<<<<<<< HEAD
   const gotUrl = "https://anapioficeandfire.com/api/books"
   // Variable that will store the api URL
   return fetch(gotUrl)
   .then(response => response.json())
   .then(json => renderBooks(json))
+=======
+  // Variable that will store the api URL
+  const gotUrl = "'https://anapioficeandfire.com/api/books"
+  fetch(gotUrl)
+  .then(response => {
+    return response.json();
+  })
+  .then(renderBooks(json) {
+  });
+>>>>>>> 056211c11ecd39e7ac601622e184dbbde25bed6c
 }
 
 function renderBooks(json) {
